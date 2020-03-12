b85f93c84a02a246b5a1ebd965c14cda055ac49c
Ben Galley <bengalley21@gmail.com>
2020-03-12 17:03:35 +0000

    refactored update method to model and added .find method to return populated fields for edit feature

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -25,10 +25,32 @@ describe Bookmark do
   end
 
   context '.delete' do
-    it 'deletes Bookmarks' do
+    it 'deletes bookmarks' do
       bookmark = Bookmark.add(title: 'codewars', url: 'https://www.codewars.com/dashboard')
       Bookmark.delete(id: bookmark.id)
       expect(Bookmark.all.length).to eq 0
+    end
+  end
+
+  context '.update' do
+    it 'updates bookmarks' do
+      bookmark = Bookmark.add(title: 'makers', url: 'https://makers.tech')
+      updated_bookmark = Bookmark.update(id: bookmark.id, title: 'Snakers Academy', url: 'https://snakers.tech')
+      expect(updated_bookmark).to be_a Bookmark
+      expect(updated_bookmark.id).to eq bookmark.id
+      expect(updated_bookmark.title).to eq('Snakers Academy')
+      expect(updated_bookmark.url).to eq('https://snakers.tech')
+    end
+  end
+
+  context '.find' do
+    it 'returns the requested bookmark object' do
+      bookmark = Bookmark.add(title: 'makers', url: 'https://makers.tech')
+      result = Bookmark.find(id: bookmark.id)
+      expect(result).to be_a Bookmark
+      expect(result.id).to eq bookmark.id
+      expect(result.title).to eq('makers')
+      expect(result.url).to eq('https://makers.tech')
     end
   end
 end
